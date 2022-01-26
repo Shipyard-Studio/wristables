725c057a898aac70f36500f7adf4daa29279d583
dd0sxx <contacttheotelonis@gmail.com>
2022-01-26 11:44:07 -0700

    added spec

--- contracts/Wristables.sol
+++ contracts/Wristables.sol
@@ -6,12 +6,31 @@ import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Wristables is ERC721A, PaymentSplitter, Ownable {
+    /// @dev spec:
+    /// ERC721(a) contract with 555 unique tokens.
+    /// Dutch Auction style mint. 
+    /// Starts at 3 ETH per token dropping by 0.5 ETH every 5 minutes until sell-out (base price of 0.25ETH).
+    /// Payout primary sales to addresses based on pre-determined %.
+    /// Payout royalties to addresses based on pre-determined %.
+    /// ERC2981 compliant.
+    /// (Maybe) Should have an airdrop function that only the owner can call.
 
+
+contract Wristables is ERC721A, PaymentSplitter, Ownable {
+    /// @dev `maxBatchSize` refers to how much a minter can mint at a time.
+    /// @dev See `PaymentSplitter.sol` for documentation on `payees` and `shares_`.
     constructor (uint256 maxBatchSize_, address[] memory payees, uint256[] memory shares_) 
-    ERC721A("Wristables", "WRISTS", maxBatchSize_) 
+    ERC721A("Wristables", "WRST", maxBatchSize_) 
     PaymentSplitter( payees, shares_) {
 
+    }
+
+    /// @dev override token uri to append .json to string
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory json = ".json";
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), json)) : "";
     }
 
 }
