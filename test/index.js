c01656503eac96c9a15d2e11d9ca7eb8859b31c1
dd0sxx <contacttheotelonis@gmail.com>
2022-02-04 13:06:02 -0700

    owner test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,22 @@
 const { expect } = require("chai");
 const { ethers, upgrades } = require("hardhat");
 
-describe("Wristables Proxy", function () {
-  let Wristables,
-    wristables,
-    WristablesV2,
-    wristablesV2,
+let Wristables,
+  wristables,
+  WristablesV2,
+  wristablesV2,
+  shipyardWallet,
+  imagineWallet,
+  wristablesWallet,
+  addr2,
+  addr3,
+  addr4,
+  addr5,
+  addr6,
+  addrs;
+
+async function proxyDeploy() {
+  [
     shipyardWallet,
     imagineWallet,
     wristablesWallet,
@@ -14,47 +25,32 @@ describe("Wristables Proxy", function () {
     addr4,
     addr5,
     addr6,
-    addrs;
+    ...addrs
+  ] = await ethers.getSigners();
 
-  beforeEach(async function () {
+  Wristables = await ethers.getContractFactory("Wristables");
+  WristablesV2 = await ethers.getContractFactory("WristablesV2");
+  // how we would do it if this weren't UUPS
+  // wristables = await Wristables.deploy();
+
+  // how we do it instead
+  wristables = await upgrades.deployProxy(
+    // contract to deploy as proxy:
+    Wristables,
+    // this array is where arguments given to initializer go:
     [
-      shipyardWallet,
-      imagineWallet,
-      wristablesWallet,
-      addr2,
-      addr3,
-      addr4,
-      addr5,
-      addr6,
-      ...addrs
-    ] = await ethers.getSigners();
-    // ShipToken = await ethers.getContractFactory("ShipToken");
-    // shipToken = await ShipToken.deploy(owner.address, []);
+      [shipyardWallet.address, imagineWallet.address, wristablesWallet.address],
+      [7, 2, 1],
+    ],
+    // Here we indicate this is a UUPS:
+    { kind: "uups" }
+  );
 
-    Wristables = await ethers.getContractFactory("Wristables");
-    WristablesV2 = await ethers.getContractFactory("WristablesV2");
-    // how we would do it if this weren't UUPS
-    // wristables = await Wristables.deploy();
+  await wristables.deployed();
+}
 
-    // how we do it instead
-    wristables = await upgrades.deployProxy(
-      // contract to deploy as proxy:
-      Wristables,
-      // this array is where arguments given to initializer go:
-      [
-        [
-          shipyardWallet.address,
-          imagineWallet.address,
-          wristablesWallet.address,
-        ],
-        [7, 2, 1],
-      ],
-      // Here we indicate this is a UUPS:
-      { kind: "uups" }
-    );
-
-    await wristables.deployed();
-  });
+describe("Wristables Proxy", function () {
+  beforeEach(proxyDeploy);
 
   it("Should deploy as a proxy successfully", async function () {
     expect(await wristables.MAX_SUPPLY()).to.equal(9999); // quick deploy check
@@ -67,7 +63,21 @@ describe("Wristables Proxy", function () {
     expect(await wristablesV2.version(), "V2");
   });
 
-  it("Should run a test", async function () {
+  it("Should have an owner", async function () {
+    expect(await wristables.owner()).to.deep.equal(shipyardWallet.address);
+  });
+});
+
+describe("Wristables Contract Unit Tests", function () {
+  beforeEach(proxyDeploy);
+
+  it("Should airdrop", async function () {
+    // await wristables.airdrop()
+  });
+
+  it("Should batch airdrop", async function () {
     // your test here!
   });
+
+
 });
