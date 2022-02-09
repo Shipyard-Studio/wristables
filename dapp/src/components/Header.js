785b6f0241bbcea002788b4fc43740a5f2dc3d59
dd0sxx <contacttheotelonis@gmail.com>
2022-02-09 09:37:42 -0800

    added flex box to the header, removed absolute position

--- dapp/src/components/Header.js
+++ dapp/src/components/Header.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 import '../style/Header.css';
 //import backgroundVideo from './test.mp4';
 
 function Header ({walletAddress, connect}) {
   return (
     <div className="header">
-        <div>
-            <h1>Wristable</h1>
-            {/* <p><b>BY</b> WRIST AFICIONADO</p> */}
-        </div> 
-        <div className='nav-logo-flex'>
-            <a href="https://discord.com" target="_blank" alt="discord"><img className="nav-logos" src="/discord.png" /></a>
-            <a href="https://instagram.com/wristaficionado/" target="_blank" alt="instagram"><img className="nav-logos" src="/instagram.png" /></a>
-            <a href="https://twitter.com/wristaficionado/" target="_blank" alt="twitter"><img className="nav-logos" src="/twitter.png" /></a>
-        </div>
-        {/* <div onClick={connect} className='address'>
-        {walletAddress.length > 0 ? `${walletAddress.slice(0,6)}...${walletAddress.slice(-4)}` :
-        'CONNECT'
+        <div className='nav-container'>
+            <div style={{width: 150, height: 50, marginRight: '2%'}}/>
+            <div >
+                <h1>Wristable</h1>
+                {/* <p><b>BY</b> WRIST AFICIONADO</p> */}
+            </div> 
+            <div className='nav-logo-flex'>
+                <a href="https://discord.com" target="_blank" alt="discord" rel="noreferrer"><img className="nav-logos" src="/discord.png" /></a>
+                <a href="https://instagram.com/wristaficionado/" target="_blank" alt="instagram" rel="noreferrer"><img className="nav-logos" src="/instagram.png" /></a>
+                <a href="https://twitter.com/wristaficionado/" target="_blank" alt="twitter" rel="noreferrer"><img className="nav-logos" src="/twitter.png" /></a>
+            </div>
+            {/* <div onClick={connect} className='address'>
+            {walletAddress.length > 0 ? `${walletAddress.slice(0,6)}...${walletAddress.slice(-4)}` :
+            'CONNECT'
         }</div> */}
+        </div>
         <br /><br /><br /><br /><br /><br />
         <div>
             <h2>The official watch maker of the Metaverse.</h2>
