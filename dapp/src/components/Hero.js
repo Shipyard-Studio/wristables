402c2cb0e4685bc102af5655f484aa7c6ebcc989
dd0sxx <contacttheotelonis@gmail.com>
2022-02-16 18:12:15 -0500

    added mobile first styling

--- dapp/src/components/Hero.js
+++ dapp/src/components/Hero.js
@@ -5,18 +5,21 @@ import '../style/Hero.css'
 const Hero = () => {
 
     return (
-        <div className='hero'>
+        <div className='hero h-full'>
             <Header />
-            <div className='ml-32 mt-32'>
+            <div className='flex flex-col justify-center m-auto h-full w-10/12 mt-[-100px]'>
                 <div className='small-text text-slate-300'>Welcome to the Wrist Aficianado watch club.</div>
                 <div className='big-text'>
-                    Watch maker of<br/>
-                    the <span className='yellow-text'>Metaverse</span><br/>
+                    Watch maker of
+                    {console.log(window.innerWidth)}
+                    {window.innerWidth < 640 ? <> </> : <br/>}
+                    the <span className='yellow-text'>Metaverse</span>
+                    {window.innerWidth < 640 ? <> </> : <br/>}
                     <span className='green-text'>March 5th.</span>
                 </div>
-                <div className='flex w-1/5 justify-between'>
+                <div className='flex w-4/12 md:w-2/12 justify-between'>
                     <a href='https://discord.gg/pcfaMQSFfW' target='_blank' alt='discord link'>
-                        <img className='mt-5 w-6/12 h-2/12 bg-cover' src='/WASiteAssets/DiscordButton.png' alt='discord button' />
+                        <img className='md:mt-5 bg-cover' src='/WASiteAssets/DiscordButton.png' alt='discord button' />
                     </a>
                     {/* <div>Mint</div> */}
                 </div>
