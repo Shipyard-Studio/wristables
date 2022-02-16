ed74fee71296dac641c2aafe79476f853e29ea5c
dd0sxx <contacttheotelonis@gmail.com>
2022-02-16 11:52:24 -0500

    added html parser and finished text sections

--- dapp/src/components/TextSection.js
+++ dapp/src/components/TextSection.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactHtmlParser from "react-html-parser";
 
 const TextSection = ({props}) => {
+
+    let body = ReactHtmlParser(props.body)
 
     return (
         <div className='m-auto w-4/12'>
                 <div className='big-text'>{props.header}</div>
-                <div className='small-text'>{props.body}</div>
+                <div className='small-text'>{body}</div>
         </div>
     )
 
