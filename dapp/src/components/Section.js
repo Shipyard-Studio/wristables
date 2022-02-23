6f260e508ba8a67d2427014254495779313b83a0
dd0sxx <contacttheotelonis@gmail.com>
2022-02-23 10:14:36 -0700

    fixed parallax

--- dapp/src/components/Section.js
+++ dapp/src/components/Section.js
@@ -26,7 +26,7 @@ const Section = ({bg, size, Component, componentProps}) => {
             bgImage={bg ? bg : null}
             bgImageAlt="background image"
             bgImageSize="100%"
-            strength={-100}
+            strength={200}
         >
                 {Component ? <Component props={componentProps}/> : <></>}
         </Parallax>
