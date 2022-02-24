3e74e0382f675e713759735423b0bb849e12686d
dd0sxx <contacttheotelonis@gmail.com>
2022-02-24 12:36:15 -0700

    added thank you

--- dapp/src/components/TextSection.js
+++ dapp/src/components/TextSection.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactHtmlParser from "react-html-parser";
 import axios from 'axios';
 
 const TextSection = ({props}) => {
+
+    let emailText = (status) => {
+        let input = document.getElementById('email-input')
+        
+        status ?
+            input.value = 'Thanks!'
+        :
+            input.value =  'There was an issue...'
+    }
 
     let body = ReactHtmlParser(props.body)
 
@@ -20,10 +29,13 @@ const TextSection = ({props}) => {
                 {
                     email: input.value
                 })
+                console.log(req.status)
+                emailText(true)
                 return true
             }
         catch (err) {
             console.error(err)
+            emailText(false)
             return false
         }
     }
@@ -35,7 +47,7 @@ const TextSection = ({props}) => {
                 {props.emailCapture ?
                 <div className='w-1/12 flex align-center justify-between pt-2'>
                     <form>
-                        <input id='email-input' className='p-3 text-neutral-300 bg-neutral-800' placeholder="join our mailing list" style={{outline: 'none'}}/>
+                        <input id='email-input' className='p-3 text-neutral-300 bg-neutral-800' placeholder='join our mailing list' style={{outline: 'none'}}/>
                     </form>
                     <div onClick={postEmail} className='p-2 px-5 bg-amber-400 text-black text-xl hover:cursor-pointer'>→</div>
                 </div>
