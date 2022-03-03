7a6654846d723a4aac6a9c16ce47684a675d1af2
dd0sxx <contacttheotelonis@gmail.com>
2022-03-03 11:39:23 -0700

    changed compliler version to 8.2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,7 +28,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   // solidity: "0.8.4",
   solidity: {
-    version: "0.8.4",
+    version: "0.8.2",
     settings: {
       optimizer: {
         enabled: true,
