087f2f3a2eecb1d533c03c9372c30ee65c38c7da
dd0sxx <contacttheotelonis@gmail.com>
2022-03-04 15:21:53 -0700

    positioned the counter correctly

--- dapp/src/components/Hero.js
+++ dapp/src/components/Hero.js
@@ -32,8 +32,8 @@ const Hero = ({props}) => {
                 verifyWallet()
             }
             if (verified === true) {
-                await redeem() 
-                // await mint() //switch to mint when the public mint goes live
+                // await redeem() 
+                await mint() //switch to mint when the public mint goes live
             }
             if (verified === false) {
                 // display error
@@ -55,6 +55,8 @@ const Hero = ({props}) => {
         }
     }
 
+    console.log(window.contract)
+
     return (
         <div className='hero h-full bg-zinc-900'>
             <Watch />
@@ -63,14 +65,19 @@ const Hero = ({props}) => {
                 <div className='big-text text-center z-10 noselect'>
                     Wrist Aficionado<br/>Watch Club.
                 </div>
-                <div className='flex flex-col lg:flex-row w-10/12 md:w-3/5 lg:1/2 justify-between mx-auto z-10 pb-0 lg:pb-10'>
-                    <a href='https://discord.gg/pcfaMQSFfW' className='whitespace-nowrap ease-in ease-out duration-300 hover:bg-blue-600  mx-auto lg:mx-2 mt-5 lg:mt-0 m-auto lg:m-none  w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-zinc-600 text-center rounded-full py-2 lg:py-5 hover:cursor-pointer' target='_blank' alt='discord link'>Discord</a>
-                    <div 
-                    onClick={handleClick}
-                    className='whitespace-nowrap hover:cursor-pointer ease-in ease-out duration-300 m-auto mx-auto lg:mx-2 mt-5 lg:mt-0 lg:m-none w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-lime-600 hover:bg-lime-500 text-center rounded-full py-2 lg:py-5' src='/WASiteAssets/DiscordButton.png' alt='discord button' >
-                        {props.walletAddress.length > 0 ? mintText() : 'Connect Wallet'}
-                        </div>
-                    <div onClick={props.openModal} className='whitespace-nowrap hover:bg-blue-600 ease-in ease-out duration-300 m-auto  mx-auto lg:mx-2 mt-5 lg:mt-0 lg:m-none mb-10 lg:mb-0 w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-zinc-600 text-center rounded-full py-2 lg:py-5 hover:cursor-pointer' src='/WASiteAssets/DiscordButton.png' alt='discord button' >Piece Unique Studio</div>
+                <div>
+                    <div className='flex flex-col lg:flex-row w-10/12 md:w-3/5 lg:1/2 justify-between mx-auto z-10 pb-0 lg:pb-10'>
+                    <div className='m-auto'>0/1000</div>
+                    </div>
+                    <div className='flex flex-col lg:flex-row w-10/12 md:w-3/5 lg:1/2 justify-between mx-auto z-10 pb-0 lg:pb-10'>
+                        <a href='https://discord.gg/pcfaMQSFfW' className='whitespace-nowrap ease-in ease-out duration-300 hover:bg-blue-600  mx-auto lg:mx-2 mt-5 lg:mt-0 m-auto lg:m-none  w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-zinc-600 text-center rounded-full py-2 lg:py-5 hover:cursor-pointer' target='_blank' alt='discord link'>Discord</a>
+                        <div 
+                        onClick={handleClick}
+                        className='whitespace-nowrap hover:cursor-pointer ease-in ease-out duration-300 m-auto mx-auto lg:mx-2 mt-5 lg:mt-0 lg:m-none w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-lime-600 hover:bg-lime-500 text-center rounded-full py-2 lg:py-5' src='/WASiteAssets/DiscordButton.png' alt='discord button' >
+                            {props.walletAddress.length > 0 ? mintText() : 'Connect Wallet'}
+                            </div>
+                        <div onClick={props.openModal} className='whitespace-nowrap hover:bg-blue-600 ease-in ease-out duration-300 m-auto  mx-auto lg:mx-2 mt-5 lg:mt-0 lg:m-none mb-10 lg:mb-0 w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-zinc-600 text-center rounded-full py-2 lg:py-5 hover:cursor-pointer' src='/WASiteAssets/DiscordButton.png' alt='discord button' >Piece Unique Studio</div>
+                    </div>
                 </div>
             </div>
         </div>
