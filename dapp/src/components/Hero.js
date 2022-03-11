31cb4249bf2a8c729e8bbb36555f8c12541b0ae5
dd0sxx <contacttheotelonis@gmail.com>
2022-03-11 11:29:05 -0700

    updated discord link

--- dapp/src/components/Hero.js
+++ dapp/src/components/Hero.js
@@ -16,7 +16,7 @@ const Hero = ({props}) => {
                     Wrist Aficionado<br/>Watch Club.
                 </div>
                 <div className='flex flex-col lg:flex-row w-10/12 md:w-3/5 lg:1/2 justify-between mx-auto z-10 pb-0 lg:pb-10'>
-                    <a href='https://discord.gg/pcfaMQSFfW' className='whitespace-nowrap ease-in ease-out duration-300 hover:bg-blue-600  mx-auto lg:mx-2 mt-5 lg:mt-0 m-auto lg:m-none  w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-zinc-600 text-center rounded-full py-2 lg:py-5 hover:cursor-pointer' target='_blank' alt='discord link'>Discord</a>
+                    <a href='https://discord.com/invite/cJpYAvJhTG' className='whitespace-nowrap ease-in ease-out duration-300 hover:bg-blue-600  mx-auto lg:mx-2 mt-5 lg:mt-0 m-auto lg:m-none  w-3/5 lg:w-1/2 md:mt-5 bg-cover text-center bg-zinc-600 text-center rounded-full py-2 lg:py-5 hover:cursor-pointer' target='_blank' alt='discord link'>Discord</a>
                     <div 
                     onClick={null} // disabled
                     // onClick={props.connect} //for live site
