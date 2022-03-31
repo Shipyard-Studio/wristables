52cc91e9b33ead1529b88de3b8911e0debe37552
dd0sxx <contacttheotelonis@gmail.com>
2022-03-31 18:35:20 -0400

    changed copy in text sections

--- dapp/src/components/TextSection.js
+++ dapp/src/components/TextSection.js
@@ -51,7 +51,7 @@ const TextSection = ({props}) => {
     }
 
     return (
-        <div className='m-auto h-full flex flex-col justify-center w-10/12 md:w-4/12'>
+        <div className='text-section m-auto h-full flex flex-col justify-center w-10/12 md:w-4/12'>
                 <div className='big-text noselect'>{props.header}</div>
                 <div className='small-text'>{body}</div>
                 {props.emailCapture ?
