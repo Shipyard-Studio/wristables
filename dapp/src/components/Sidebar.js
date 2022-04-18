a7a0ec1dee0640cda1aeee6c21a22a090f28e7bc
dd0sxx <contacttheotelonis@gmail.com>
2022-04-18 15:39:03 -0600

    fixed styling for new sidebar

--- dapp/src/components/Sidebar.js
+++ dapp/src/components/Sidebar.js
@@ -6,8 +6,9 @@ import '../style/Sidebar.css';
 const Sidebar = () => {
   return (
     <Menu customBurgerIcon={ <img src="/assets/Menu Icon.png" /> }>
-      <a href="https://wristaficionado.com/" target='_blank' rel="noreferrer" className='menu-item text-center px-4 py-2 border-2 border-blue-600 rounded-full hover:bg-blue-600 hover:cursor-pointer z-10'>Visit Boutique</a>
       
+    <img className='w-3/5 bm-center' src='/WAlogo.png' alt='wawc logo'/>
+
       <Link
       to="home"
       activeClass="active"
@@ -54,9 +55,12 @@ const Sidebar = () => {
       spy={true}
       smooth={true}
       offset={-70}
-      duration={500}>
+      duration={500}
+      >
       Roadmap
       </Link>
+
+    <a href="https://wristaficionado.com/" target='_blank' rel="noreferrer" className='bm-center hover-invert whitespace-nowrap ease-in ease-out duration-300 hover:underline-offset-2 m-auto w-10/12 bg-cover text-center border-2 py-2 lg:py-5 hover:cursor-pointer z-10'>Visit Boutique</a>
 
       <div className='' style={{display: 'flex !important', flexDirection: 'row'}}>
         <a href="https://twitter.com/wristaficionado/" className='m-auto menu-item z-10 invert' target="_blank" alt="discord" rel="noreferrer"><img className="nav-logos" src={process.env.PUBLIC_URL + '/discord.png'} /></a>
