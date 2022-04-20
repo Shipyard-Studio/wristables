ebe7dc4150382100e0ca08b69ffb5cb010b41d8d
dd0sxx <contacttheotelonis@gmail.com>
2022-04-20 16:04:34 -0600

    added new roadmap image and styling

--- dapp/src/components/Roadmap.js
+++ dapp/src/components/Roadmap.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import ReactHtmlParser from "react-html-parser";
+import '../style/Roadmap.css';
 
 const Roadmap = ({props}) => {
 
     let body = ReactHtmlParser(props.body)
 
     return (
-        <div className='md:mx-20' id={props.header.toLowerCase()}>
+        <div className='md:mx-20 lg:mt-20' id={props.header.toLowerCase()}>
+            <img src='/roadmap-combined.png' className='absolute mt-20'/>
             <div className='m-auto w-full h-full flex pb-10 justify-between'>
-                <div className='flex flex-col w-5/12 justify-center'>
-                    <div className='big-text text-left noselect pb-10'>{props.header}</div>
-                    <div className='small-text'>{body}</div>
+                <div className='flex flex-col w-full justify-center m-auto'>
+                    <div className='big-text text-left noselect pb-10 m-auto'>{props.header}</div>
+                    <div className='small-text text-left'>{body}</div>
                 </div>  
             </div>
         </div>
