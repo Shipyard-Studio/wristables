fd556d943e954ad443bef2f04df2d2da2dea955c
dd0sxx <contacttheotelonis@gmail.com>
2022-04-21 10:20:35 -0600

    removed made in the imagination room

--- dapp/src/components/Footer.js
+++ dapp/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="h-auto w-150 p-10 flex flex-col items-center justify-center bg-darkgray">
             <div className='text-center pb-2'>© 2022 Wrist Aficionado Watch Club. All rights reserved.</div>
             <a className='text-center pb-2' href='./TERMS.pdf' target='blank'>Terms and Conditions</a>
-            <a href='https://www.imaginationroom.io/' target='_blank' className='text-slate-300'>Made in the Imagination Room</a>
+            {/* <a href='https://www.imaginationroom.io/' target='_blank' className='text-slate-300'>Made in the Imagination Room</a> */}
         </div>
       );
 };
