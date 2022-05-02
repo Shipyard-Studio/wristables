36fc7ad10b7ecc6f352aab9b317cd90227cfe1e7
dd0sxx <contacttheotelonis@gmail.com>
2022-05-02 08:25:13 -0600

    updated g-tag and copy in piece unique modal form

--- dapp/src/components/ModalForm.js
+++ dapp/src/components/ModalForm.js
@@ -30,12 +30,12 @@ const ModalForm = ({closeModal, pageWidth}) => {
             <h2 className='z-20 pt-8 text-center medium-text'>Piece Unique Studio</h2>
             <video src='/assets/Background Video 04 - Mobile 1080p 8MBps.mp4' className='z-20 mx-auto' alt='fingerprint' />
             <p className='z-20 small-text w-full px-4 pb-2 mx-auto '>
-            The first {1 + ',' + '000'} owners of our hypercar watch will also have the opportunity to purchase 1 of 1,000 Piece Uniques that can be customized right here in our studio.
+            The first {5 + ',' + '00'} owners of our hypercar watch will also have the opportunity to purchase 1 of 100 Piece Uniques that can be customized right here in our studio.
             <br/>
             Your Piece Unique will appear blank in your wallet until you have customized your watch in our studio.
             <br />
             <br />
-            (Customization starts at 20ETH)
+            (Customization starts at 5ETH)
             </p>
             </div>
         }
