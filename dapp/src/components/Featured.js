ad1c911b64b39bba6fa7fa992643474d41dcfce4
dd0sxx <contacttheotelonis@gmail.com>
2022-05-16 10:07:51 -0400

    added bg video, fixing style to make text more legible

--- dapp/src/components/Featured.js
+++ dapp/src/components/Featured.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Featured = () => {
+
+    const classString = 'mx-auto absolute z-auto absolute'
+
+    const [offsetX, setOffsetX] = useState(0)
+
+    function calcOffset () {
+        setOffsetX((1750 - window.innerWidth) / 2)
+    }
+
+    useEffect(() => {
+        calcOffset()
+        window.addEventListener('resize', calcOffset)
+    }, [])
+
     return (
-        <div className='w-full h-full mx-auto flex items-center absolute z-auto'>
+        <div className='w-full h-full mx-auto flex items-center absolute -z-50'>
            <video id='vid' className={classString} style={{minWidth: 1750, right: -offsetX}}  loop={true} autoPlay="autoplay" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto">
                 <source src='/WATCH_FACTORY_SOUNDSCAPE.mp4' type="video/mp4" />
             </video>
