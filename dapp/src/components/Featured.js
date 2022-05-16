d735da81d115a551dcadf1727fc5a3ddb403ad52
dd0sxx <contacttheotelonis@gmail.com>
2022-05-16 09:54:23 -0400

    added full screen code, need to go back to other branch

--- dapp/src/components/Featured.js
+++ dapp/src/components/Featured.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Featured = () => {
     return (
-        <div className='w-full'>
-            <video loop={true} autoPlay="autoplay" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto">
+        <div className='w-full h-full mx-auto flex items-center absolute z-auto'>
+           <video id='vid' className={classString} style={{minWidth: 1750, right: -offsetX}}  loop={true} autoPlay="autoplay" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto">
                 <source src='/WATCH_FACTORY_SOUNDSCAPE.mp4' type="video/mp4" />
             </video>
         </div>
