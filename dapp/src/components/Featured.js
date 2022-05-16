c71c3a4e1dc876ffb346dcc43f8db032f6d458e0
dd0sxx <contacttheotelonis@gmail.com>
2022-05-16 09:38:13 -0400

    removed border

--- dapp/src/components/Featured.js
+++ dapp/src/components/Featured.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Featured = () => {
     return (
-        <div className='w-full border'>
+        <div className='w-full'>
             <video loop={true} autoPlay="autoplay" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto">
                 <source src='/WATCH_FACTORY_SOUNDSCAPE.mp4' type="video/mp4" />
             </video>
