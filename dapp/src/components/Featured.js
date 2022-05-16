57e901d6efec62b42957d6eb34731029942d48b1
dd0sxx <contacttheotelonis@gmail.com>
2022-05-16 13:30:20 -0400

    fixed overflowing video

--- dapp/src/components/Featured.js
+++ dapp/src/components/Featured.js
@@ -16,7 +16,7 @@ const Featured = () => {
     }, [])
 
     return (
-        <div className='w-full h-full mx-auto flex items-center absolute -z-50'>
+        <div className='w-full h-full mx-auto flex items-center absolute -z-50 overflow-hidden'>
            <video id='vid' className={classString} style={{minWidth: 1750, right: -offsetX}}  loop={true} autoPlay="autoplay" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto">
                 <source src='/WATCH_FACTORY_SOUNDSCAPE.mp4' type="video/mp4" />
             </video>
