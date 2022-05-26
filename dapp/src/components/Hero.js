5341a23b15aa9b4b7abea8ebdf1c10ab116537a5
dd0sxx <contacttheotelonis@gmail.com>
2022-05-26 10:30:54 -0600

    updated package.json and deleted unused watch component

--- dapp/src/components/Hero.js
+++ dapp/src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import Watch from './Watch';
+// import Watch from './Watch';
 import Video from './Video';
 import Featured from './Featured';
 
