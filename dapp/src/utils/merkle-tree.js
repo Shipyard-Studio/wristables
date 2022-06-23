cb56d9dc43b8821b18f83a59c9f4c30714e7e96f
dd0sxx <contacttheotelonis@gmail.com>
2022-06-22 21:02:36 -0700

    added new merkle tree

--- dapp/src/utils/merkle-tree.js
+++ dapp/src/utils/merkle-tree.js
@@ -1,27 +1,11 @@
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
+const whiteList = require("./wl");
 
-const dataRaw = [
-  "0xbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef",
-  "0xfeedfeedfeedfeedfeedfeedfeedfeedfeedfeed",
-  "0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
-  "0x7Eb696df980734DD592EBDd9dfC39F189aDc5456",
-];
-
-const leaves = dataRaw.map((x) => keccak256(x));
+const leaves = whiteList.map((x) => keccak256(x));
 
 const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 
-const getProof = (leaf) => {
-  return merkleTree.getHexProof(keccak256(leaf))
-}
-
-const verify = (proof, leaf) => {
-  return merkleTree.verify(proof, keccak256(leaf), merkleTree.getHexRoot())
-
-}
-
 module.exports = {
-  getProof,
-  verify
+  merkleTree,
 };
