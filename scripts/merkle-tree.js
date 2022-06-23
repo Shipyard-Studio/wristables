bf3c9e78a420c992ce186f65320da58b1ab90a42
dd0sxx <contacttheotelonis@gmail.com>
2022-06-22 20:59:13 -0700

    added official whitelist

--- scripts/merkle-tree.js
+++ scripts/merkle-tree.js
@@ -1,14 +1,8 @@
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
+const whiteList = require("./wl");
 
-const dataRaw = [
-  "0xbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef",
-  "0xfeedfeedfeedfeedfeedfeedfeedfeedfeedfeed",
-  "0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
-  "0x7Eb696df980734DD592EBDd9dfC39F189aDc5456",
-];
-
-const leaves = dataRaw.map((x) => keccak256(x));
+const leaves = whiteList.map((x) => keccak256(x));
 
 const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 
